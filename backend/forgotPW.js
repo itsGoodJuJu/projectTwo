9376a939dd01e2156623db252a506d9a19827c3e
COR Coding <corcoding@Coding-9.localdomain>
2024-04-25 10:11:37 -0400

    PT-3: I created the 'Forgot Password' Page.

--- backend/forgotPW.js
+++ backend/forgotPW.js
@@ -6,11 +6,13 @@ var number = document.getElementById("number");
 var length = document.getElementById("length");
 let password1 = document.getElementById('newPW');
 let password2 = document.getElementById('newPW2');
-
+var lowerCaseLetters = /[a-z]/g;
+var upperCaseLetters = /[A-Z]/g;
+var numbers = /[0-9]/g;
 
     password1.onkeyup = function() {
         // Validate lowercase letters
-        var lowerCaseLetters = /[a-z]/g;
+        
         if(password1.value.match(lowerCaseLetters)) {
           letter.classList.remove("invalid");
           letter.classList.add("valid");
@@ -20,7 +22,7 @@ let password2 = document.getElementById('newPW2');
       }
       
         // Validate capital letters
-        var upperCaseLetters = /[A-Z]/g;
+        
         if(password1.value.match(upperCaseLetters)) {
           capital.classList.remove("invalid");
           capital.classList.add("valid");
@@ -30,7 +32,7 @@ let password2 = document.getElementById('newPW2');
         }
       
         // Validate numbers
-        var numbers = /[0-9]/g;
+        
         if(password1.value.match(numbers)) {
           number.classList.remove("invalid");
           number.classList.add("valid");
@@ -48,7 +50,20 @@ let password2 = document.getElementById('newPW2');
           length.classList.add("invalid");
         }
       }
-     if (password1.value === password2.value) {
+      if(password1.value.match(lowerCaseLetters) === false) {
+        alert(" at least 1 lowercase letter required ")
+      } else if(password1.value.match(upperCaseLetters) === false) {
+        alert("at least 1 uppercase letter required ")
+      } else if(password1.value.match(numbers) === false) {
+        alert("at least 1 number required ") 
+      }  else if (password1.value === "") {
+        alert("password field is empty") 
+       } else if(password1.value.length < 8) {
+        alert("password must have 8 characters")
+      } else if (password2.value === "") {
+        alert("please re-enter password") 
+      }
+       else if (password1.value === password2.value) {
         alert("Passwords match")
      } else if( password1 != password2.value) {
         alert("Passwords do not match")
